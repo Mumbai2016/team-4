f82dee3ee475d4f39323bb1c7a31aff2df382929
parikshithR <parikshith_rai@yahoo.com>
2016-07-24 04:39:47 +0530

    piechart done

--- piechart.php
+++ piechart.php
@@ -3,31 +3,56 @@
 
 	header("Content-type: image/png");
 	
-	$data = array();
+	$servername = "ec2-54-179-133-213.ap-southeast-1.compute.amazonaws.com";
+	$username = "root";
+	$password = "root";
+	$dbname = "DreamADream";
+
+	// Create connection
+
+
+	$conn = mysqli_connect("127.0.0.1", $username, $password, $dbname);
+	
+	
+
+	// Check connection
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	
 	$chart = new PieChart(500, 500);
-
+	
+	
 	$dataSet = new XYDataSet();
-	mysql_connect("127.0.0.1", "root", "root") or die (mysql_error ());
-
+	
 	// Select database
-	mysql_select_db("DreamADream") or die(mysql_error());
+	
 	$date = 2012;
 	// SQL query
+	
 	while($date<2017){
-	$strSQL = "SELECT count(person_id) as count_row FROM Person WHERE joined_date > '".$date."-01-01' AND joined_date< '".$date."-12-31' ";
-
-	// Execute the query (the recordset $rs contains the result)
-	$rs = mysql_query($strSQL);
-	// Loop the recordset $rs
-	// Each row will be made into an array ($row) using mysql_fetch_array
-	$row = mysql_fetch_array($rs)
-	 
-	$dataSet->addPoint(new Point($date,$row[count_row]));
+	$strSQL = "SELECT count(person_id) as count_row FROM Person WHERE joined_date > '".$date."-01-01' AND joined_date < '".$date."-12-31' ";
+	//$strSQL ="SELECT * FROM Person ";
+	
+	
+	$result = mysqli_query($conn, $strSQL);
+	
+	
+		
+		
+		
+	$row = mysqli_fetch_assoc($result); 
+	
+		
+	$dataSet->addPoint(new Point($date+"",$row[count_row]));
 	$chart->setDataSet($dataSet);
+	
+	
 	$date++;
 	}
 	
 
 	
 	$chart->render();
+	
 ?>
