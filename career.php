1d63bda9def3a7598b123ae60c6da996ff2526a8
Rupesh <chavan11rupesh02@gmail.com>
2016-07-23 21:59:46 +0530

    career

---
+++ career.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+	<title>CSS Text</title>
+
+	<link rel="stylesheet" href="css-text.css" type="text/css" media="screen" />
+	
+</head>
+<style>
+body { 
+	margin:0; 
+	padding:0;
+	font:62.5% "Helvetica Neue", Helvetica, Arial, sans-serif;
+	color:#000;
+	text-align:center;
+	}
+	
+div,
+h1, h2, h3, h4, h5, h6,
+p,
+ul, ol, li,
+blockquote {
+	margin:0;
+	padding:0;
+	}
+	
+	div.text {
+		width:50em;
+		padding:2em 0;
+		margin:auto;
+		text-align:left;
+		}
+	
+		h1, h2, h3, h4, h5, h6 {
+			font-family:Georgia, "Times New Roman", Times, serif;
+			line-height:1.2;
+			}
+		
+		h1 {
+			font-size:3.4em;
+			margin-bottom:.8em;
+			}
+		
+		h2 {
+			font-size:2.4em;
+			padding-bottom:.2em;
+			border-bottom:1px solid #333;
+			margin-top:1em;
+			margin-bottom:.7em;
+			}
+		
+		h3 {
+			font-size:1.8em;
+			margin-bottom:.7em;
+			}
+		
+		p {
+			font-size:1.3em;
+			line-height:1.5;
+			margin-bottom:1.3em;
+			}
+		
+		ul, ol {
+			font-size:1.3em;
+			margin:0 0 1.3em 2em;
+			}
+		
+			li {
+				margin-bottom:.4em;
+				}
+			
+		blockquote {
+			padding-left:1em;
+			border-left:3px solid #999;
+			margin:2.5em 2em;
+			}
+			
+			blockquote p {
+				font:italic 1.2em/1.6 Georgia, "Times New Roman", Times, serif;
+				}
+</style>
+<body>
+
+	<div class="text">
+	
+		<h1>Career Connect Program...</h1>
+		
+		<p>
+Career Connect program, which equips 14-19 year olds with information, skills, and access to opportunities to make healthy transitions into adulthood. In the Career Connect program, DaD conducts career awareness workshops and runs short-term modules in English on communication skills, money management, and career guidance. They also provide access to internships, scholarships, vocational training and jobs. The program is delivered in two learning centers, based in Bangalore.
+
+The Career Connect program also tracks the success of their young people in the program between the ages of 15 to 22 years old. DaD provides support for program participants as needed and ensures that they are all meaningfully engaged in education, placement, working, studying, or are pursuing vocational training.
+</p>
+	
+	</div>
+
+</body>
+
+</html>
